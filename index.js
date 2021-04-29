749faf44d05ea7016775957e48d8118b765f2646
Laurel Stickney <ls60@protonmail.com>
2021-04-28 21:22:28 -0700

    First version of server written in node.js which runs Notes App.

---
+++ index.js
@@ -0,0 +1,65 @@
+// to build this server, we'll need three core modules: path, fs, and, and http
+
+const path = require('path')
+const fs = require('fs')
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+    let filePath = path.join(__dirname, 'notesApp', request.url === '/' ? 'index.html' : request.url)
+    let contentType = getContentType(filePath) || 'text/html'
+    let emptyPagePath = path.join(__dirname, 'notesApp', '404.html')
+    fs.readFile(filePath, 'utf8', (err, content) => {
+        if(err){
+            if(err.code === 'ENOENT'){ // error code for a missing file
+                fs.readFile(emptyPagePath, 'utf8', (err, content) => {
+                    response.writeHead(200, {'Content-Type': contentType})
+                    response.end(content)
+                })
+            } else {
+                response.writeHead(500)
+                response.end('A server error has occured.')
+            }
+        }
+        if(!err){
+            response.writeHead(200, {'Content-Type': contentType})
+            response.end(content)
+        }
+    }) 
+    // if (request.url === '/'){
+    //     let filePath = path.join(__dirname, 'notesApp', 'index.html')
+    //     fs.readFile(filePath, 'utf8', (err, data) => {
+    //        response.writeHead(200, {'Content-Type': 'text/html'})
+    //        response.end(data) 
+    //     })    
+    // }
+
+    // if (request.url === '/index2.html'){
+    //     let filePath = path.join(__dirname, 'notesApp', 'index2.html')
+    //     fs.readFile(filePath, 'utf8', (err, data) => {
+    //        response.writeHead(200, {'Content-Type': 'text/html'})
+    //        response.end(data) 
+    //     })    
+    // }
+})
+
+const getContentType = (filePath) => {
+    let extname = path.extname(filePath)
+    if (extname === '.js'){
+        return 'text/javascript'
+    }
+    if (extname === '.css'){
+        return 'text/css'
+    }
+    if (extname === '.png'){
+        return 'image/png'
+    }
+    if (extname === '.jpg'){
+        return 'image/jpg'
+    }
+}
+
+const port = 5000
+
+server.listen(port, () =>{
+    console.log(`Server is running on port ${port}`)
+})
